6bb5b07cf2018c284b94c3e596e326150fd7a7a9
gustavo_honda <gustavo_honda@usp.br>
2024-03-14 13:57:08 -0300

    Add transparent circle

--- math_plot.py
+++ math_plot.py
@@ -7,8 +7,10 @@ screen_dim = np.array([1000,1000])
 screen_certer = np.array([500,500])
 black = np.array([0,0,0])
 white = np.array([255,255,255])
-blue = np.array([0,0,255])
 red = np.array([255,0,0])
+green = np.array([0,255,0])
+blue = np.array([0,0,255])
+dark_green = np.array([100,150,50])
 
 
 
@@ -34,15 +36,12 @@ def timer(screen):
     time_text = my_font.render(str(i) , False, black)
     screen.blit(time_text,np.array([0,0]))
 
-def decent_line(screen):
-    t = pg.time.get_ticks()
-    line_start = screen_certer  
+def line(screen):
     i = 100
     j = 200
+    line_start = screen_certer  
     line_end =  screen_certer+ [i,j]
-    print(line_end)
     pg.draw.line(screen,black,line_start,line_end)
-    pg.display.flip()
 
 def clock(screen):
     t = pg.time.get_ticks()
@@ -50,9 +49,7 @@ def clock(screen):
     j = np.sin(t/100)* 500
     line_start = screen_certer  
     line_end =  screen_certer+ [i,j]
-    print(line_end)
     pg.draw.line(screen,blue,line_start,line_end)
-    pg.display.flip()
 
 def reverse_clock(screen):
     t = pg.time.get_ticks()
@@ -60,23 +57,34 @@ def reverse_clock(screen):
     j = np.cos(t/100)* 500
     line_start = screen_certer  
     line_end =  screen_certer+ [i,j]
-    print(line_end)
     pg.draw.line(screen,red,line_start,line_end)
-    pg.display.flip()    
+
+
+def alpha_circle(screen):
+    r = 200
+    target_rect = pg.Rect(screen_certer, (0, 0)).inflate((r * 2, r * 2))
+    surf = pg.Surface(target_rect.size, pg.SRCALPHA)
+    surf.set_alpha(80)
+    pg.draw.circle(surf, dark_green, (r, r), r)
+    screen.blit(surf, target_rect)
+
+def circle(screen):
+    pg.draw.circle(screen,center=screen_certer,color=green,radius=100)
 
 def main():
     pg.init()
-    screen= pg.display.set_mode(screen_dim)
+    screen = pg.display.set_mode(screen_dim)
     screen.fill(color=white)
     while True:
         screen.fill(white)
         entries()
         timer(screen)
-        decent_line(screen)
+        line(screen)
         clock(screen)
         reverse_clock(screen)
-
-    
+        circle(screen)
+        alpha_circle(screen)
+        pg.display.flip()
 
 if __name__ == '__main__':
     main()
