cf4e5e32cf9accd22b8edbf44d05f536ecb474f2
gustavo_honda <gustavo_honda@usp.br>
2024-03-14 14:39:49 -0300

    rainbow generator

--- math_plot.py
+++ math_plot.py
@@ -13,6 +13,19 @@ blue = np.array([0,0,255])
 dark_green = np.array([100,150,50])
 
 
+Red    = np.array([255,0,0]);
+Orange = np.array([255,127,0]);
+Yellow = np.array([255,255,0]);
+Green  = np.array([0,255,0]);
+Blue   = np.array([0,0,255]);
+Indigo = np.array([75,0,130]);
+Violet = np.array([148,0,211]);
+Trunk  = np.array([165,42,42]);
+Grass  = np.array([80,250,80]);
+Leaf   = np.array([80,150,80]);
+rainbow = [Red,Orange,Yellow,Green,Blue, Indigo,Violet,Trunk,Grass,Leaf]
+
+
 
 def quit():
     sys.exit()
@@ -71,19 +84,31 @@ def alpha_circle(screen):
 def circle(screen):
     pg.draw.circle(screen,center=screen_certer,color=green,radius=100)
 
+def rainbow_circles(screen):
+    i=0
+    while True:
+        i = i % len(rainbow)
+        pg.draw.circle(screen,center=screen_certer + [20*i,0],color=rainbow[i],radius=10)
+        i +=1
+        yield None
+        
+
 def main():
     pg.init()
     screen = pg.display.set_mode(screen_dim)
     screen.fill(color=white)
+    gen = rainbow_circles(screen)
     while True:
-        screen.fill(white)
+        # screen.fill(white)
         entries()
         timer(screen)
-        line(screen)
-        clock(screen)
-        reverse_clock(screen)
-        circle(screen)
-        alpha_circle(screen)
+        # line(screen)
+        # clock(screen)
+        # reverse_clock(screen)
+        # circle(screen)
+        # alpha_circle(screen)
+        # next(gen)
+        next(gen)
         pg.display.flip()
 
 if __name__ == '__main__':
